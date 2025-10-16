6dca801bc96c09f249a91dd6203525ebea3267d3
afsha <147975801+afshamsayyed@users.noreply.github.com>
2025-10-16 21:59:53 +0530

    Add files via upload

---
+++ experiment1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main (){
+    /*
+    Name: afsha
+    age: 17
+    Class: comps 01
+    Division: C
+    UIN: 251P062
+    */
+
+   printf ("Name: afsha \n");
+   printf ("age: 17 \n");
+   printf ("Class: comps01 \n");
+   printf ("Division: C\n");
+   printf ("UIN: 251P062 \n");
+
+  return 0;
+}
