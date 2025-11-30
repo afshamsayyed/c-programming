1eb5a4e53451d58cbbfdfe9f77db4b3d55bddd1d
afsha <147975801+afshamsayyed@users.noreply.github.com>
2025-11-30 21:32:37 +0530

    Add files via upload

---
+++ experiment7a[1].c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*Afsha sayyed
+uin: 251P062
+roll no: 54
+comps*/
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
