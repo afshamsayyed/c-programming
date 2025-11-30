2c4a08b074bef2992ab007ae869f40343f16238a
afsha <147975801+afshamsayyed@users.noreply.github.com>
2025-11-30 21:41:55 +0530

    Add files via upload

---
+++ experiment10a[1].c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* afsha sayyed
+Uin:251P062
+roll no:54
+comps*/
+int main()
+{
+    int a, b, sum;
+    int *p1, *p2;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    sum = *p1 + *p2;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
